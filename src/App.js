5fe587230eaeea57bf956dec816d723bc6627773
Aman kumar <arup.chandra@jumkey.com>
2021-05-27 11:45:27 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>
+      This is the home page!
+    </p>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      This is the about page!
+    </p>
+  </div>
+)
+
+const Contact = ({ match }) => (
+  <div>
+    <h2>Contact</h2>
+    <p>
+      This is the contact us page!
+    </p>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
     </div>
-  );
-}
-
+  </Router>
+)
 export default App;
